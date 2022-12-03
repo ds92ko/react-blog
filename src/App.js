58d6de0152baa5dab404a68a7e2a85420a8021c9
Dasom Ko <ds92ko@gmail.com>
2022-12-03 16:37:12 +0900

    Part 1. 4회차 숙제 완료

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import { useState } from 'react';
 
 function App() {
   let post = '강남 우동 맛집'; // 서버에서 가져온 데이터라고 가정
-  let [글제목, b] = useState('여자 코트 추천');
+  let [titleList, setTitleList] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div className="list">
-        <h4>{글제목}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {titleList.map(item => {
+        return (
+          <div className="list">
+            <h4>{item}</h4>
+            <p>2월 17일 발행</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
